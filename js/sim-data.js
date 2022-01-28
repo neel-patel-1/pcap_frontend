326cc9db7a2e529f67f782f5f4aa66e5525d50a6
neelpatelbiz <neelpatelbiz@gmail.com>
2022-01-27 21:24:19 -0600

    packet stream simulation

--- js/sim-data.js
+++ js/sim-data.js
@@ -1,6 +1,12 @@
 
 common_subNets_8 = ["192.168", "10.0"]
 prots = ["FTP", "TELNET", "SMTP", "DNS", "DHCP", "HTTP"]
+
+/*
+@pre None
+@post None
+@return Sample Packet with src IP, dst IP, and protocol
+*/
 function SamplePacket(){
     let packet = {
         srcIp: (Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255)) +"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255)),
@@ -9,3 +15,19 @@ function SamplePacket(){
     }
     return packet
 }
+
+/*
+@pre None
+@param packetResponder is a function taking a packet object as an argument
+@post packetResponder is called upon arrival of a packet
+@return none
+*/
+function packetStream(packetResponder){
+    let genPacket = new Promise( (resolve, reject) =>{
+        setTimeout( () => {resolve(SamplePacket())}, Math.random()*2000 + 250 )
+    })
+    genPacket.then( (packet) => {packetResponder(packet); packetStream(packetResponder)} )
+}
+
+function packResponse(packet){console.log(packet)}
+packetStream(packResponse)
